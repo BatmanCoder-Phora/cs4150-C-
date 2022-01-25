e35c3ae52b45d88f094e6efdd18f8178997e77b6
BatmanPhora-coder <u1216135@utah.umail.edu>
2022-01-25 10:59:04 -0700

    add problem set two code

---
+++ Problem Set 2/Ps2submission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_2
+{
+    public class Ps2submission
+    {
+        public void main(string[] args)
+        {
+
+        }
+
+        public void alTwo()
+        {
+
+        }
+
+    }
+}
