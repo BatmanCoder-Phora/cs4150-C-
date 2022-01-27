4e47628246a427eedb7cf55a53c158fcd32c413b
BatmanPhora-coder <u1216135@utah.umail.edu>
2022-01-27 00:52:26 -0700

    added some very base code to ps2.

--- Problem Set 2/Ps2submission.cs
+++ Problem Set 2/Ps2submission.cs
@@ -8,9 +8,18 @@ namespace Problem_Set_2
 {
     public class Ps2submission
     {
+        /// <summary>
+        /// The main function for a program that finds the minium vales in a circle array. 
+        /// </summary>
+        /// <param name="args"></param>
         public void main(string[] args)
         {
+            int miniPosition = 0;
+            //Console.ReadLine();
 
+
+            Console.WriteLine("minimum" + miniPosition);
+            Console.ReadLine();
         }
 
         public void alTwo()
