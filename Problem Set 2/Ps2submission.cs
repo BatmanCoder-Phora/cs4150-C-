b09966cc74abfba3a0a4cbe18e49c23f35eba465
BatmanPhora-coder <u1216135@utah.umail.edu>
2022-01-28 03:08:02 -0700

    trying to get base test working, haven't started on edge tests.

--- Problem Set 2/Ps2submission.cs
+++ Problem Set 2/Ps2submission.cs
@@ -13,76 +13,90 @@ namespace Problem_Set_2
         static public int lastsec;
         static public int firstslope = 1;
         static public int Lastslope = 0;
+        static public int min = int.MaxValue;
+        static public int first;
+        static public int secfirst;
         /// <summary>
         /// The main function for a program that finds the minium vales in a circle array. 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int miniPosition;
             // read the array size
             string line = Console.ReadLine();
             Int32.TryParse(line, out int arrayLength);
             arrayLength -= 1;
 
             // get the first point and last point only once. 
-            int first = queryAraay(0);
-            int secfirst = queryAraay(0 + 1);
-            last = queryAraay(arrayLength);
-            lastsec = queryAraay(arrayLength - 1);
-            firstslope = secfirst - first;
+             first = queryAraay(0);
+             secfirst = queryAraay(0 + 1);
+             last = queryAraay(arrayLength);
+             lastsec = queryAraay(arrayLength - 1);
+             firstslope = secfirst - first;
 
             // find the min 
-            miniPosition = algorithmToFindMin(0,(arrayLength));
-            
+            algorithmToFindMin(0, (arrayLength));
+
 
             // print the poistion of where the minimum is minimum 
-            Console.WriteLine("minimum" + " " + miniPosition);
+            Console.WriteLine("minimum" + " " + min);
             Console.WriteLine("number of queries" + " " + qCounter);// TESTS TAKE IT OUT 
         }
         /// <summary>
-        /// An algorithm to find 
+        /// An algorithm to find the position of the minimum value.
         /// </summary>
         /// <param name="arrayLength"></param>
         /// <exception cref="NotImplementedException"></exception>
-        private static int algorithmToFindMin(int start,int arrayLength)
+        private static void algorithmToFindMin(int start, int arrayLength)
         {
-            // to many q   
-            int min = 0;
+            // to many q  
+            int mid, midTwo;
             int midcal = (start + arrayLength) / 2;
-            int mid = queryAraay(midcal);
-            int midTwo = queryAraay(((start + arrayLength)/ 2)-1);
-            qCounter += 2;
+
+            if (midcal == start || (midcal+1) == start+1)
+            { mid = first; midTwo = secfirst; }
+            else if (midcal == arrayLength || (midcal+1) == arrayLength-1)
+            { mid = last; midTwo = lastsec; }
+            else
+            {
+                mid = queryAraay(midcal);
+                midTwo = queryAraay(midcal - 1);
+                qCounter += 2;
+            }
+
+            if (midcal == start || midcal == arrayLength)
+                return;
+
             int midslope = (mid - midTwo);
 
             // how to even check the slope 
             if (midslope < 0)
             {
-                if(Lastslope > 0)
-                {
-                    min = Lastslope;
-                }
-                else
+                if(!(Lastslope < 0))
                 {
                     firstslope = midslope;
-                    min = algorithmToFindMin(midcal + 1, arrayLength);
+                    if (midTwo < mid && midTwo < min)
+                        min = midcal-1;
+                    else if (mid < min)
+                        min = midcal;
+                    algorithmToFindMin(midcal, arrayLength);                  
                 }
+                
             }
             if (midslope > 0)
             {
-                if (firstslope < 0)
-                {
-                    min = firstslope;
-                }
-                else
+                if (!(firstslope > 0))
                 {
                     Lastslope = midslope;
-                    min = algorithmToFindMin(midcal + 1, arrayLength);
+                    if (midTwo < min && midTwo<mid)
+                        min = midcal - 1;
+                    else 
+                        min = midcal;
+                    algorithmToFindMin(start , midcal);
                 }
+                
             }
-        //    if (midslope < 0 && Lastslope < 0)
-            
-          return min;
+            // spepical edge cases. 
         }
         /// <summary>
         /// Helpful method to make queries. 
