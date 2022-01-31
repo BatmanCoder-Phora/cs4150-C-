7b5664b63245c1ee3ce641fc602fd0de0852fcdc
BatmanPhora-coder <u1216135@utah.umail.edu>
2022-01-31 13:07:26 -0700

    created problem set three. Added the mian with basic reading from console code and creat a method number for finding the min number of shops.

---
+++ Problem Set 3/Ps3submission.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Author: Sephora Bateman 
+ * Class: CS 4150
+ * Problem Set #3
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_3
+{
+    public class Ps3submission
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int numOfDestinations;
+            int numOfFerryRoutes;
+            string line;
+
+            line = Console.ReadLine();
+            string[] linespilt = line.Split(' ');
+            numOfDestinations = Int32.Parse(linespilt[0]);
+            numOfFerryRoutes = Int32.Parse(linespilt[1]);
+
+            for (int i = 0; i < numOfFerryRoutes; i++)
+            {
+                line = Console.ReadLine();
+                findMinNumOfShops(line);
+            }
+
+            Console.WriteLine("");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="line"></param>
+        public  static void findMinNumOfShops(string line)
+        {
+
+        }
+    }
+}
