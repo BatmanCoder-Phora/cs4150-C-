78e800e15be68bcd6fdcfe57d594c556b0957f9f
BatmanPhora-coder <u1216135@utah.umail.edu>
2022-02-03 15:28:24 -0700

    figuring out how to input data.

--- Problem Set 3/Ps3submission.cs
+++ Problem Set 3/Ps3submission.cs
@@ -15,6 +15,7 @@ namespace Problem_Set_3
     {
         public static long nofDestinations;
         public static long passedDes = 0;
+        public static int minislands;
         public static Dictionary<int,int> islandsAndFerries = new Dictionary<int, int>();
         public static HashSet<int> islands = new HashSet<int>();
         public static long numOfDestinations;
@@ -34,25 +35,34 @@ namespace Problem_Set_3
             numOfDestinations = Int32.Parse(linespilt[0]);
             numOfFerryRoutes = Int32.Parse(linespilt[1]);
 
-            for (int i = 0; i < numOfFerryRoutes; i++)
-            {
-                inputIslandAndFerryData(line, i);
-               
-            }
-            findMinNumOfShops(islands,passedDes,index);
-        }
+            for (int i = 1; i < numOfFerryRoutes; i++)
+                inputIslandAndFerryData(i); // store the islands and their data into some data sturctures. 
+            
 
-        private static void inputIslandAndFerryData(string line, int i)
+            findMinNumOfShops(islands,passedDes,index);
+
+            // Print the min number of islands and the islands it takes. 
+            Console.WriteLine(minislands);
+            for (int i = 1; i <= numOfDestinations; i++)
+                if ((passedDes & (1 << i)) != 0)
+                    Console.WriteLine(i + ' ');
+        }
+        /// <summary>
+        ///  Stores the islands and the ferry routes in a dictionary also figure out the destination. 
+        /// </summary>
+        /// <param name="i"></param>
+        private static void inputIslandAndFerryData(int i)
         {
-            line = Console.ReadLine();
+            string line = Console.ReadLine();
             string[] spilt = line.Split(' ');
             int island = Int32.Parse(spilt[0]);
             int ferry = Int32.Parse(spilt[1]);
             if (i <= numOfDestinations)
                 nofDestinations = nofDestinations | (1 << i);
 
-            if (islandsAndFerries.ContainsKey(island))
-                islandsAndFerries.Add(island, 0);
+            // inculde orphan islands. 
+            if (!islandsAndFerries.ContainsKey(island))
+                islandsAndFerries.Add(island, ferry);
             else
             {
                 islandsAndFerries.TryGetValue(island, out int value);
@@ -60,16 +70,30 @@ namespace Problem_Set_3
                 islandsAndFerries.Add(island, (value | (1 << ferry)));
             }
         }
-
+        /// <summary>
+        /// Finds the smallest number of shops that need to be built. 
+        /// </summary>
+        /// <param name="islands"> the list of islands.</param>
+        /// <param name="passedDes">The islands we have passed</param>
+        /// <param name="index">the island we are checking.</param>
         private static void findMinNumOfShops(HashSet<int> islands, long passedDes, int index)
         {
-            if (passedDes == nofDestinations)
+            if (index > numOfDestinations) //
+               return;
+
+            if (passedDes == nofDestinations) // base case
                 return;
 
+            islandsAndFerries.TryGetValue(index, out int value);
+            for (int i = 1; i <= numOfDestinations; i++)
+                if (((value & (1 << i)) != 0) && (passedDes & (1 << i)) !=1)
+                    passedDes = passedDes | (1 << i);
 
-            findMinNumOfShops(islands,passedDes, index + 1);
+            // also inculde the orphan islands. 
+
+            findMinNumOfShops(islands,0, index+1);
             islands.Add(index);
-            findMinNumOfShops(islands, passedDes, index + 1);
+            findMinNumOfShops(islands, passedDes, index+1);
         }
     }
 }
