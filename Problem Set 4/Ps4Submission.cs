45c0dfc100bb74796194a0e936ae9f961be95f7e
BatmanCoder-Phora <u1216135@utah.umail.edu>
2022-02-10 14:50:31 -0700

    Added problems et 4 base code

---
+++ Problem Set 4/Ps4Submission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_4
+{
+    public class Ps4Submission
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+        }
+        public static void solution()
+        {
+
+        }
+    }
+}
