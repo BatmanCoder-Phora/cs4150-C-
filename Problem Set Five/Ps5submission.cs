f0d6216fc13d1be95ff01d44cd6c897ba8af7847
BatmanCoder-Phora <u1216135@utah.umail.edu>
2022-02-26 19:43:15 -0700

    added submission.

---
+++ Problem Set Five/Ps5submission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_Five
+{
+    public class Ps5submission
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+
+}
