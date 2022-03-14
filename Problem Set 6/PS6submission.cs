3243ec7cd5b6b9d208057da56b7fa7844a35bb15
BatmanCoder-Phora <u1216135@utah.umail.edu>
2022-03-14 09:52:47 -0600

    added problem set six

---
+++ Problem Set 6/PS6submission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_6
+{
+    public class PS6submission
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
