df7821019d284dbb3327ab185f364a3111c398a9
BatmanCoder-Phora <u1216135@utah.umail.edu>
2022-03-18 13:09:59 -0600

    added som basic functionalilty

--- Problem Set 6/PS6submission.cs
+++ Problem Set 6/PS6submission.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /**
+ * CS 4150: Algorithms 
  * 
+ * Assignment Six: Adventures with firends '
+ * 
+ * Author: Sephora Bateman 
  */
 namespace Problem_Set_6
 {
     public class PS6submission
     {
+       
+       
         static void Main(string[] args)
         {
-        
+          bool soultionFound = false;
+          string printsloution = "unsolvable";
+
+
+
+            if (!soultionFound)
+                Console.WriteLine(printsloution);
+          else
+                Console.WriteLine("Print query");
+
         }
         static public void TopologicalSort()
         {
