91d3748123cb28be49f5301dcd47490adb6033b3
BatmanCoder-Phora <u1216135@utah.umail.edu>
2022-03-18 19:28:46 -0600

    trying to inout data

--- Problem Set 6/PS6submission.cs
+++ Problem Set 6/PS6submission.cs
@@ -10,32 +10,89 @@ using System.Threading.Tasks;
  * 
  * Author: Sephora Bateman 
  */
+
+/*
+ * Steps:
+ *  represents the player requried quests in two sperate graphs 
+ *  use topological sort tow find the order the player can either complete the quest 
+ *  print weather both players were able to complete the quest. 
+ */
 namespace Problem_Set_6
 {
     public class PS6submission
     {
        
-       
+       public static Dictionary<string,HashSet<string>> Graph = new Dictionary<string, HashSet<string>>();
         static void Main(string[] args)
         {
-          bool soultionFound = false;
-          string printsloution = "unsolvable";
+            //player One
+            string line = Console.ReadLine();
+            int playerOneNumOfQ = int.Parse(line);
+            string playerOne = "";
+            for (int i = 0; i < playerOneNumOfQ; i++)
+                playerOne += Console.ReadLine() + "/";
 
+            // player two
+            line = Console.ReadLine();
+            int playerTwoNumOfQ = int.Parse(line);
+            string playerTwo = "";
+            for (int i = 0; i < playerTwoNumOfQ; i++)
+                playerTwo += Console.ReadLine() + "/";
 
+            // combined quests
+            line = Console.ReadLine();
+            int comQuests = int.Parse(line);
+            for (int i = 0; i < comQuests; i++)
+                Graph.Add(Console.ReadLine().Trim(), new HashSet<string>());
 
-            if (!soultionFound)
-                Console.WriteLine(printsloution);
-          else
-                Console.WriteLine("Print query");
-
+            StoreInfomation(playerOne, playerTwo,playerOneNumOfQ,playerTwoNumOfQ);
         }
+
+        private static void StoreInfomation(string playerOne, string playerTwo, int v,int z)
+        {
+            string[] spiltOneQuests = playerOne.Split('/');
+            string[] spiltTwoQuests = playerTwo.Split('/');
+            int total = v+z;
+            for (int i = 0; i < total; i++)
+                if (i < v)
+                    inputGraphdata(spiltOneQuests[i], "1");
+                else
+                    inputGraphdata(spiltOneQuests[i], "2");
+        }
+
+        private static void inputGraphdata(string quests,string player)
+        {
+            string[] spiltline = quests.Split(" ");
+            string questA = spiltline[0];
+            string questB = spiltline[1];
+
+            if (Graph.ContainsKey(questA) && Graph.ContainsKey(questB))
+                Graph[questA].Add(questB);
+            else if (Graph.ContainsKey(questA))
+                Graph[questA].Add(questB + "-" + player);
+            else if (Graph.ContainsKey(questB))
+                Graph[questA + "-" + player].Add(questB);
+            else
+            {
+                Graph.Add(questA + "-" + player, new HashSet<string>());
+                Graph.Add(questB + "-" + player, new HashSet<string>());
+                Graph[questA + "-" + player].Add(questB + "-" + player);
+            }
+        }
+
         static public void TopologicalSort()
         {
-    
+            foreach (string s in Graph.Keys)
+            {
+                // mark them
+            }
+                  // run topSort DFS
+            
         }
         static public void TopSOrtDFS()
         {
      
         }
+        
     }
 }
